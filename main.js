6aca13c6d01bb70653a1ed4b20765a990f403e98
YTmash <159938161+YTmash@users.noreply.github.com>
2025-02-17 19:53:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,57 @@
 const team = {
+  // Private array holding player information
   _players: [
     {firstName: 'Leonard', lastName: 'Jenkins', age: 32},
     {firstName: 'Bernard', lastName: 'Rollins', age: 29},
     {firstName: 'Foghorn', lastName: 'Leghorn', age: 85}
   ],
+
+  // Private array holding game results
   _games: [
     {opponent: 'Hawks', teamPoints: 33, opponentPoints: 23},
     {opponent: 'Timberwolves', teamPoints: 45, opponentPoints: 33},
     {opponent: 'Giants', teamPoints: 43, opponentPoints: 24}
   ],
 
+  // Getter method to retrieve the list of players
   get players(){
     return this._players;
+  },
 
-    },
+  // Getter method to retrieve the list of games
+  get games() {
+    return this._games;
+  },
 
-    get games() {
-      return this._games;
+  // Method to add a new player to the team
+  addPlayer(newFirstName, newLastName, newAge) {
+    let player = {
+      firstName: newFirstName,
+      lastName: newLastName,
+      age: newAge
+    };
 
-    },
+    // Add the new player to the _players array
+    this._players.push(player);
+  },
 
-    addPlayer(newFirstName, newlastName, newAge) {
-      let player = {
-          firstName: newFirstName,
-          lastName: newlastName,
-          age: newAge
-      };
+  // Method to add a new game result
+  addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+    let game = {
+      opponent: newOpponent,
+      teamPoints: newTeamPoints,
+      opponentPoints: newOpponentPoints
+    };
 
-      this.players.push(player)
-    },
+    // Add the new game result to the _games array
+    this._games.push(game);
+  },
+};
 
-    addGame(newOpponent, newTeamPoints, newOpponentPoints) {
-      let game = {
-        opponent: newOpponent,
-        teamPoints: newTeamPoints,
-        opponentPoints: newOpponentPoints
-      };
+// Adding a new player to the team
+team.addPlayer('Fred', 'Banks', 35);
+console.log(team.players); // Logs the updated list of players
 
-      this.games.push(game)
-    },
-  };
-
-  team.addPlayer('Fred', 'Banks', 35);
-  console.log(team.players);
-
-  team.addGame('Titans', 100, 98);
-  console.log(team.games);
+// Adding a new game result
+team.addGame('Titans', 100, 98);
+console.log(team.games); // Logs the updated list of games
